a9eea86c5a3948d46127fb288c2d9179f0f23711
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2020-04-06 22:56:16 -0400

    Done

---
+++ Assignment10.py
@@ -0,0 +1,25 @@
+# Temur Khabibullaev 4/2/2020
+import re
+
+
+def remover(sentence):
+    sentence = str(sentence)
+    sentence = re.sub(r'[^\w\s]', '', sentence)
+    return sentence
+
+
+def palindrome_check(string):
+    string = string.strip()
+    string = string.lower()
+    string = remover(string)
+    if len(string) <= 1:
+        return 'yes'
+    if string[0] != string[-1]:
+        return 'no'
+    else:
+        return palindrome_check(string[1: -1])
+
+
+while True:
+    string = input('Enter the string, and I\'ll check if it is a palindrome:\n>>>')
+    print(palindrome_check(string))
